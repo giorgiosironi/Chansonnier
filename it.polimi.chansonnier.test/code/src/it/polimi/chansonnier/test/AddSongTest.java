2d727e547392d3c32f00399cdb3864a2889c846d
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2010-05-04 13:20:23 +0200

    Added httpunit

--- it.polimi.chansonnier.test/code/src/it/polimi/chansonnier/test/AddSongTest.java
+++ it.polimi.chansonnier.test/code/src/it/polimi/chansonnier/test/AddSongTest.java
@@ -1,9 +1,17 @@
 package it.polimi.chansonnier.test;
 
 import junit.framework.TestCase;
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
 
 public class AddSongTest extends TestCase {
-	public void testNothing() {
+	public void testNothing() throws Exception {
+		 WebConversation wc = new WebConversation();
+		    WebRequest     req = new GetMethodWebRequest( "http://localhost:8080/chansonnier/add" );
+		    WebResponse   resp = wc.getResponse( req );
+
 		assertTrue(true);
 	}
 }
