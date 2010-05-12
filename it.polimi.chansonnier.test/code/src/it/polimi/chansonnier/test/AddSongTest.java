b08665d6ad9f2d750ffc12a3d59b1be74c51a0ee
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2010-05-12 09:08:30 +0200

    Added new failing acceptance test (search)

--- it.polimi.chansonnier.test/code/src/it/polimi/chansonnier/test/AddSongTest.java
+++ it.polimi.chansonnier.test/code/src/it/polimi/chansonnier/test/AddSongTest.java
@@ -1,5 +1,9 @@
 package it.polimi.chansonnier.test;
 
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
 import junit.framework.TestCase;
 import com.meterware.httpunit.GetMethodWebRequest;
 import com.meterware.httpunit.PostMethodWebRequest;
@@ -16,22 +20,40 @@ public class AddSongTest extends TestCase {
 	}
 	
 	public void testGivenAYouTubeLinkAddsTheRelatedSongToTheIndex() throws Exception {
-		WebConversation wc = new WebConversation();
-		PostMethodWebRequest	req = new PostMethodWebRequest( "http://localhost:8080/chansonnier/add" );
-		req.setParameter("link", "http://www.youtube.com/watch?v=e8w7f0ShtIM");
-		WebResponse resp = wc.getResponse(req);
+		WebResponse resp = addVideoLink("http://www.youtube.com/watch?v=e8w7f0ShtIM");
 		// TODO insert redirect
 		assertTrue(resp.getText().contains("Success"));
-		for (int i = 0; i < 40; i++) {
-			Thread.sleep(30000);
-			WebConversation wc2 = new WebConversation();
-			WebRequest     req2 = new GetMethodWebRequest( "http://localhost:8080/chansonnier/last" );
-			WebResponse   resp2 = wc2.getResponse( req2 );
-			if (resp2.getText().contains("Beautiful Day")) {
+		WebRequest     req = new GetMethodWebRequest( "http://localhost:8080/chansonnier/last" );
+		assertWebPageContains(req, "Beautiful Day", 300000);
+	}
+	
+	public void testGivenAnAddedYouTubeLinkTheSongIsSearchable() throws Exception {
+		addVideoLink("http://www.youtube.com/watch?v=5tK7-OuYfJc");
+		WebRequest req = new GetMethodWebRequest( "http://localhost:8080/chansonnier/search" );
+		req.setParameter("lyrics", "I walk alone");
+		assertWebPageContains(req, "Boulevard of Broken Dreams", 300000);
+	}
+	
+	private WebResponse addVideoLink(String link) throws Exception {
+		WebConversation wc = new WebConversation();
+		PostMethodWebRequest	req = new PostMethodWebRequest( "http://localhost:8080/chansonnier/add" );
+		req.setParameter("link", link);
+		WebResponse resp = wc.getResponse(req);
+		return resp;
+	}
+	
+	private void assertWebPageContains(WebRequest req, String text, int timeout) throws InterruptedException, IOException, SAXException {
+		int tries = timeout / 10000;
+		for (int i = 0; i < tries; i++) {
+			Thread.sleep(10000);
+			WebConversation wc = new WebConversation();
+			WebResponse   resp = wc.getResponse( req );
+			if (resp.getText().contains(text)) {
 				assertTrue(true);
+				System.out.println(resp.getText());
 				return;
 			}
 		}
-		assertTrue(false);
+		fail("After " + timeout + "milliseconds of waiting, the web page does not contain the prescribed text.");
 	}
 }
