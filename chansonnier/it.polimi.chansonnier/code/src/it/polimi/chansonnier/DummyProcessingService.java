ade7981d787b55d62cd4e0a449f9c1fe55d4000e
Giorgio <giorgio@Indy.(none)>
2010-05-25 19:58:43 +0200

    Added license notices

--- chansonnier/it.polimi.chansonnier/code/src/it/polimi/chansonnier/DummyProcessingService.java
+++ chansonnier/it.polimi.chansonnier/code/src/it/polimi/chansonnier/DummyProcessingService.java
@@ -1,3 +1,9 @@
+/****************************************************************************
+ * Copyright (c) 2010 Giorgio Sironi. All rights reserved.
+ * This program and the accompanying materials are made available under 
+ * the terms of the Eclipse Public License v1.0 which accompanies this 
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ ****************************************************************************/
 package it.polimi.chansonnier;
 
 import org.eclipse.smila.blackboard.Blackboard;
