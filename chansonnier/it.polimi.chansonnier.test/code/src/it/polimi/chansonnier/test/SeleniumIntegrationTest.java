bfddf177e55d1a5305666862e8bcd32530e4bcc5
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2010-06-10 16:21:04 +0200

    Modified Selenium integration to let SeleneseTestCase be wrapped

--- chansonnier/it.polimi.chansonnier.test/code/src/it/polimi/chansonnier/test/SeleniumIntegrationTest.java
+++ chansonnier/it.polimi.chansonnier.test/code/src/it/polimi/chansonnier/test/SeleniumIntegrationTest.java
@@ -1,22 +1,30 @@
 package it.polimi.chansonnier.test;
 
+import junit.framework.TestCase;
+
 import com.thoughtworks.selenium.*;
 
-public class SeleniumIntegrationTest extends SeleneseTestCase {
+public class SeleniumIntegrationTest extends TestCase {
 	private SeleniumWrapper seleniumWrapper;
+	WrappableSeleneseTestCase wrapped;
+	Selenium selenium;
+	
 	
 	public void setUp() throws Exception {
 		seleniumWrapper = new SeleniumWrapper();
 		seleniumWrapper.start();
-		setUp("http://www.google.com/", "*chrome");
+		wrapped = new WrappableSeleneseTestCase();
+		wrapped.setUp("http://www.google.com/", "*chrome");
+		selenium = wrapped.getSelenium();
 	}
 	
-	public void tearDown() {
+	public void tearDown() throws Exception {
+		wrapped.tearDown();
 		seleniumWrapper.stop();
 	}
 	
 	public void testSeleniumRCIsStartedAndWorks() throws Exception {
 		selenium.open("/");
-		verifyTrue(selenium.isTextPresent("Google"));
+		wrapped.verifyTrue(selenium.isTextPresent("Google"));
 	}	
 }
