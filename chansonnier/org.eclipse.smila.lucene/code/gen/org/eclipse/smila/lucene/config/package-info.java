09a128844dc38fcffe6faa8303645a6a3f8bbd85
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2010-06-16 09:15:51 +0200

    Removed Lucene

--- chansonnier/org.eclipse.smila.lucene/code/gen/org/eclipse/smila/lucene/config/package-info.java
+++
@@ -1,6 +0,0 @@
-
-// CHECKSTYLE:OFF
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.eclipse.org/smila/lucene", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.eclipse.smila.lucene.config;
-
-// CHECKSTYLE:ON
